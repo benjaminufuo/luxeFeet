bfa72d81eb90515e261dbf2bd6f9bdefc21cc7c9
Taofik Ahmed <tafikahmed007@example.com>
2025-03-09 09:46:13 +0100

    updated

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -103,6 +103,45 @@ const Product = () => {
         <div className="ProductBodySmallDotBox1">
           
         </div>
+        <div className="ProductBodySmallDotBox1">
+          
+        </div>
+        <div className="ProductBodySmallDotBox1">
+          
+        </div>
+        <div className="ProductBodySmallDotBox1">
+          
+        </div>
+        
+      </div>
+
+      <div className="ProductBodySmallDescriptionBox">
+        <div className="ProductBodySmallDescriptionBoxs">DISCRIPTION</div>
+        <div className="ProductBodySmallDescriptionBoxs">MANUFATURE</div>
+        <div className="ProductBodySmallDescriptionBoxs">REVIEW</div>
+      </div>
+
+      <div className="ProductBodySmallDescriptionBoxOutline">
+        <p className="P">
+        Even the all-powerful Pointing has no control about the blind 
+        texts it is an almost unorthographic life One day however a small line of blind text
+         by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+        </p>
+
+        <p className="P">
+        When she reached the first hills of the Italic Mountains, she had a last view 
+        back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village 
+        and the subline of her own road, the Line Lane. Pityful a rethoric question ran over 
+        her cheek, then she continued her way.
+        </p>
+
+        <ul className="P2">
+          <li>The Big Oxmox advised her not to do so  </li>
+          <li>Because there were thousands of bad Commas </li>
+          <li>Wild Question Marks and devious Semikoli</li>
+          <li>She packed her seven versalia</li>
+          <li>tial into the belt and made herself on the way.</li>
+        </ul>
       </div>
 
     </div>

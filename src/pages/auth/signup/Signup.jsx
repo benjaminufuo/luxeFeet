ef5ab674bc34e578d5606b9f393e2b36695fc119
Benjamin <benjaminufuomajacob9@gmail.com>
2025-03-20 13:09:30 +0100

    validation complete

--- src/pages/auth/signup/Signup.jsx
+++ src/pages/auth/signup/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, use } from "react";
+import React, { useState, useContext, use, useEffect } from "react";
 import "../login/login.css";
 import "../signup/signup.css";
 import toast, { Toaster } from "react-hot-toast";
@@ -8,6 +8,13 @@ import { userSignUp } from "../../../api/Api";
 const Signup = () => {
   const navigate = useNavigate();
   const [disabled, setIsDisabled] = useState(true);
+  const [errorMessage, setErrorMessage] = useState({
+    fullName: "",
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [input, setInput] = useState({
     fullName: "",
     email: "",
@@ -16,18 +23,60 @@ const Signup = () => {
     confirmPassword: "",
   });
 
+  const validateField = (name, value) => {
+    let error = "";
+    if (name === "fullName") {
+      if (!value.trim()) {
+        error = "Full name is required";
+      } else {
+        const names = value.split(" ");
+        for (let i = 0; i < names.length; i++) {
+          if (names[i].charAt(0) !== names[i].charAt(0).toUpperCase()) {
+            error = "Each name must start with a capital letter";
+            break;
+          }
+        }
+      }
+    }
+    if (name === "email") {
+      if (!value.trim()) {
+        error = "Email is required";
+      } else if (value.length < 6 || value.length > 60) {
+        error = "Email should be between 6 and 60 characters";
+      } else if (!validateEmail(value)) {
+        error = "Please enter a valid email address";
+      }
+    }
+
+    if (name === "password") {
+      if (!value.trim()) {
+        error = "Password is required";
+      } else if (value.length < 6 || value.length > 60) {
+        error = "Password dhould be between 6 and 60 characters";
+      }
+    }
+
+    if (name === "confirmPassword") {
+      if (value !== input.password) {
+        error = "Passwords do not match";
+      }
+    }
+
+    if (name === "username") {
+      if (!value.trim()) {
+        error = "Username is required";
+      } else if (value.length < 6 || value.length > 20) {
+        error = "Username should be between 6 and 20 characters";
+      }
+    }
+
+    setErrorMessage((prev) => ({ ...prev, [name]: error }));
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setInput((prev) => ({ ...prev, [name]: value }));
-    setIsDisabled(
-      !(
-        input.fullName &&
-        input.email &&
-        input.username &&
-        input.password &&
-        input.confirmPassword
-      )
-    );
+    validateField(name, value);
   };
 
   const handleSignup = async (e) => {
@@ -48,13 +97,35 @@ const Signup = () => {
         navigate
       );
       if (success) {
-        navigate("/login");
+        navigate("/verify");
       }
     } catch (error) {
       console.error("Signup failed:", error.response?.data || error.message);
-      setIsDisabled(false);
     }
   };
+
+  const validateEmail = (input) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(input);
+  };
+
+  useEffect(() => {
+    const { fullName, email, username, password, confirmPassword } = input;
+    if (
+      fullName.trim() !== "" &&
+      validateEmail(email) &&
+      username.trim() !== "" &&
+      password.trim() !== "" &&
+      password.length >= 6 &&
+      confirmPassword.trim() !== "" &&
+      password === confirmPassword
+    ) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [input]);
+
   return (
     <main className="signupnmain">
       <Toaster />
@@ -68,12 +139,16 @@ const Signup = () => {
             <h2 className="label">Full Name</h2>
             <input
               className="logininput"
-              type="fullName"
+              type="text"
               name="fullName"
               value={input.fullName}
               placeholder="Enter full name"
               onChange={handleChange}
+              onBlur={(e) => validateField(e.target.name, e.target.value)}
             />
+            {errorMessage.fullName && (
+              <p className="error">{errorMessage.fullName}</p>
+            )}
           </div>
           <div className="inputcontainer">
             <h2 className="label">Email Address</h2>
@@ -84,7 +159,11 @@ const Signup = () => {
               type="email"
               value={input.email}
               onChange={handleChange}
+              onBlur={(e) => validateField(e.target.name, e.target.value)}
             />
+            {errorMessage.email && (
+              <p className="error">{errorMessage.email}</p>
+            )}
           </div>
           <div className="inputcontainer">
             <h2 className="label">Username</h2>
@@ -95,7 +174,11 @@ const Signup = () => {
               value={input.username}
               placeholder="Enter Username"
               onChange={handleChange}
+              onBlur={(e) => validateField(e.target.name, e.target.value)}
             />
+            {errorMessage.username && (
+              <p className="error">{errorMessage.username}</p>
+            )}
           </div>
           <div className="inputcontainer">
             <h2 className="label">Password</h2>
@@ -106,7 +189,11 @@ const Signup = () => {
               type="password"
               value={input.password}
               onChange={handleChange}
+              onBlur={(e) => validateField(e.target.name, e.target.value)}
             />
+            {errorMessage.password && (
+              <p className="error">{errorMessage.password}</p>
+            )}
           </div>
           <div className="inputcontainer">
             <h2 className="label">Confirm Password</h2>
@@ -117,7 +204,11 @@ const Signup = () => {
               type="password"
               value={input.confirmPassword}
               onChange={handleChange}
+              onBlur={(e) => validateField(e.target.name, e.target.value)}
             />
+            {errorMessage.confirmPassword && (
+              <p className="error">{errorMessage.confirmPassword}</p>
+            )}
           </div>
 
           <div className="register">
@@ -131,7 +222,6 @@ const Signup = () => {
             <button
               className="loginbtn"
               type="submit"
-              onClick={() => navigate("/verify")}
               disabled={disabled}
               style={{ backgroundColor: disabled ? "#b8bab8" : "#88c8bc" }}
             >
